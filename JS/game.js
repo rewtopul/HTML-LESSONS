932433ef631b5dea3e92088e4076b4f7171f729c
sergeyts <sergeysill@gmail.com>
2019-12-06 19:24:14 +0200

    first commit

--- JS/game.js
+++ JS/game.js
@@ -1,1 +1,15 @@
-game.js
+var number;// четыре цифры нашего числа
+var attempts = 0; //число попыток
+
+// общая формула - min + Math.random() * (max - min)
+number = Math.round(1000 + Math.random() * (9999 - 1000));
+guessNumber();
+
+function guessNumber() {
+	attempts++;
+	// var result=parseInt("Введите число(-1 - закончить игру)" + number, o)):
+	var result = parseInt(prompt ("Введите число(-1 - закончить игру)" ));
+	// игрок угадал число
+	
+}
+	
