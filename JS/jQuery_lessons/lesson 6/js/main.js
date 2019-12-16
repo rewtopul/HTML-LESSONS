89fefdff341fd9965cd940322ddda23185edeb38
sergeyts <sergeysill@gmail.com>
2019-12-16 19:13:08 +0200

    first commit

---
+++ JS/jQuery_lessons/lesson 6/js/main.js
@@ -0,0 +1,18 @@
+/*============ Фильтры ================*/
+
+/*  Фильтры форм в jQuery   */
+
+
+$('input').css('border', '1px solid red'); // Все поля input
+//$('input:button').css('border', '1px solid red');//Поле button - простая кнопка
+//$('input:radio').css('display', 'block'); // Радио-кнопки
+//$('input:checkbox').css('dicplay', 'block'); // Чебоксары
+//$('input:text'),css('border', '1px solid red');// Текстовые поля для ввода
+//$('input:password').css('border', '1px solid red');// Поледля ввода пароля
+//$('input:submit').css('border', '1px solid red');// Кнопка "отправить"
+//$('input:reset').css('border', '1px solid red');// Кнопка "Сбросить"
+//$('input:image').css('border', '1px solid red');// Кнопка-картинка
+//$('input:focus').css('display', 'bloc');// Выбранное поле(состояние фокус)
+//$('input:checked').css('border', '1px solid red');// Выбранный чекбокс или радио-кнопка
+//$('input:enabled').css('border', '1px solid red');// Доступное поле
+//$('input:disabled').css('border', '1px solid red');// Недоступное поле
